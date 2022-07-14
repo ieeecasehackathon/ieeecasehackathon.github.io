25a7b5ffe25f0f21d8b68e31a753c96bf45aa470
ICMDLab <dehaoliu@qq.com>
2022-07-14 17:15:11 -0400

    Update

--- archive/IMECE2020/references/problem-2/old-dataset/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Movies.zip
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/In-situ%20Meas%20Data.zip.sha256
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Movies.zip.sha256
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Build%20Command%20Data.zip.sha256
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Metadata.zip.sha256
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Metadata.zip
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/Build%20Command%20Data.zip
-wget https://opendata.nist.gov/pdrsrv/85196AB9232E7202E053245706813DFA2044/In-situ%20Meas%20Data.zip
